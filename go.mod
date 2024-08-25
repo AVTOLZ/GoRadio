75fa6351cd61ed7998fbc892876df4b73b797e35
gulp1n <abeldenouden@hotmail.com>
2024-08-25 11:51:45 +0200

    tested + fixed

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/AVTOLZ/GoRadio
 
 go 1.23.0
 
-require github.com/robfig/cron v1.2.0 // indirect
+require github.com/robfig/cron v1.2.0
--- main.go
+++ main.go
@@ -1,31 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"os/exec"
 	"time"
+
 	"github.com/robfig/cron"
 )
 
 
 
 func main() {
-	scheduler := cron.New()
+	fmt.Println("Starting Scheduler...")
+
+	scheduler := cron.NewWithLocation(time.Now().Location())
 
 	scheduler.AddFunc("0 0 10 * * 1-5", break1)
 	scheduler.AddFunc("0 50 11 * * 1-5", break2)
 	scheduler.AddFunc("0 50 13 * * 1-5", break3)
+
+	scheduler.Start()
+
+	fmt.Println("Scheduler Running")
+
+	select {}
 }
 
 func break1() {
+	fmt.Println("Break 1 Playing")
 	PlayRadio(20)
+	fmt.Println("Break 1 End")
 }
 
 func break2() {
+	fmt.Println("Break 2 Playing")
 	PlayRadio(30)
+	fmt.Println("Break 2 End")
 }
 
 func break3() {
+	fmt.Println("Break 3 Playing")
 	PlayRadio(10)
+	fmt.Println("Break 3 End")
 }
 
 func PlayRadio(minutes time.Duration) {
